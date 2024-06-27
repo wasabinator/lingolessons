680945a1d575b25def26c20566253dc2647b1d77
Tony Miceli <6946957+wasabinator@users.noreply.github.com>
2024-06-27 23:02:07 +1000

    iOS: Fix koin setup

--- client/composeApp/src/iosMain/kotlin/Helper.kt
+++ client/composeApp/src/iosMain/kotlin/Helper.kt
@@ -3,10 +3,8 @@ import com.lingolessons.di.appModule
 import com.lingolessons.domain.di.domainModule
 import org.koin.core.context.startKoin
 
-class Helper {
-    fun initKoin() {
-        startKoin {
-            modules(appModule, domainModule, platformModule, dataModule)
-        }
+fun initKoin() {
+    startKoin {
+        modules(appModule, domainModule, platformModule, dataModule)
     }
 }
