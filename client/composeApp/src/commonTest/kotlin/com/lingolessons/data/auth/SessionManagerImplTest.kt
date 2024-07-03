62b8538a281ceeeebf86ab735a7a37bd3295d4d0
Tony Miceli <6946957+wasabinator@users.noreply.github.com>
2024-07-03 23:18:29 +1000

    Complete session manager tests

--- client/composeApp/src/commonTest/kotlin/com/lingolessons/data/auth/SessionManagerImplTest.kt
+++ client/composeApp/src/commonTest/kotlin/com/lingolessons/data/auth/SessionManagerImplTest.kt
@@ -2,9 +2,15 @@ package com.lingolessons.data.auth
 
 import com.lingolessons.domain.auth.SessionManager
 import com.lingolessons.domain.auth.SessionState
+import dev.mokkery.answering.calls
 import dev.mokkery.answering.returns
+import dev.mokkery.answering.throws
 import dev.mokkery.every
+import dev.mokkery.everySuspend
+import dev.mokkery.matcher.any
 import dev.mokkery.mock
+import dev.mokkery.verify
+import dev.mokkery.verify.VerifyMode.Companion.exactly
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestCoroutineScheduler
@@ -13,6 +19,8 @@ import kotlinx.coroutines.test.runTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 class SessionManagerImplTest {
     private lateinit var tokenApi: TokenApi
@@ -36,9 +44,13 @@ class SessionManagerImplTest {
         tokenRepository = mock {
             every {
                 get()
-            }.returns(
-                tokenState
-            )
+            } returns tokenState
+
+            every {
+                put(any())
+            } calls {
+                tokenState.value = it.args.first() as SessionTokens
+            }
         }
 
         sessionManager = SessionManagerImpl(
@@ -61,4 +73,39 @@ class SessionManagerImplTest {
         assertEquals(SessionState.Authenticated("username"), sessionManager.get().value)
     }
 
+    @Test
+    fun `given a successful login there should be a session`() = runTest {
+        everySuspend {
+            tokenApi.login("user123", "password")
+        } returns (LoginResponse("a token"))
+
+        sessionManager.login("user123", "password")
+        scheduler.advanceUntilIdle()
+
+        verify(exactly(1)) {
+            tokenRepository.put(SessionTokens("user123", "a token", "TODO"))
+        }
+
+        assertEquals(SessionState.Authenticated("user123"), sessionManager.get().value)
+    }
+
+    @Test
+    fun `given a failed login there should be no session`() = runTest {
+        everySuspend {
+            tokenApi.login("user123", "password")
+        } throws Exception()
+
+        val result = runCatching {
+            sessionManager.login("user123", "password")
+            scheduler.advanceUntilIdle()
+        }
+
+        assertTrue { result.isFailure }
+        
+        verify(exactly(0)) {
+            tokenRepository.put(any())
+        }
+
+        assertNull(sessionManager.get().value)
+    }
 }
