2fb71bb0763552872c556323fa994f5b8f55745b
Tony Miceli <6946957+wasabinator@users.noreply.github.com>
2024-09-01 17:06:31 +1000

    Android: Fix SqlDriver injection

--- client/composeApp/src/androidMain/kotlin/com/lingolessons/data/di/PlatformModule.android.kt
+++ client/composeApp/src/androidMain/kotlin/com/lingolessons/data/di/PlatformModule.android.kt
@@ -1,10 +1,11 @@
 package com.lingolessons.data.di
 
+import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import com.lingolessons.data.db.AppDatabase
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 actual val platformModule = module {
-    single { AndroidSqliteDriver(AppDatabase.Schema, androidContext(), "database.db") }
+    single<SqlDriver> { AndroidSqliteDriver(AppDatabase.Schema, androidContext(), "database.db") }
 }
