fea1c07db3b35f8b6414037cd0cf976a9f0c28db
Tony Miceli <6946957+wasabinator@users.noreply.github.com>
2024-09-09 13:39:08 +1000

    KN gtk4 binding test

--- client/nativeApp/src/linuxMain/kotlin/main.kt
+++ client/nativeApp/src/linuxMain/kotlin/main.kt
@@ -2,6 +2,12 @@ import com.lingolessons.domain.lesson.SaveLesson
 import kotlinx.cinterop.ExperimentalForeignApi
 
 import gtk4.*
+import kotlinx.cinterop.COpaquePointer
+import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.StableRef
+import kotlinx.cinterop.asStableRef
+import kotlinx.cinterop.reinterpret
+import kotlinx.cinterop.staticCFunction
 
 private const val APP_ID = "com.lingolessons.app"
 
@@ -9,9 +15,29 @@ private const val APP_ID = "com.lingolessons.app"
 fun main() {
     val lesson = SaveLesson()
 
-    //val app = Application(APP_ID, ApplicationFlags.FLAGS_NONE)
-    val x = gSignalConnect(
+    val app: CPointer<GtkApplication>? =
+        gtk_application_new(
+            application_id = "com.lingolessons.app",
+            flags = G_APPLICATION_DEFAULT_FLAGS
+        )
 
+    val callback = StableRef.create(staticCFunction { source: COpaquePointer,
+                                                      data: COpaquePointer ->
+        println("*** CALLED BACK ***")
+        data.asStableRef<() -> Unit>().get().invoke()
+    })
+
+    gtk_signal_connect(
+        instance = app,
+        detailed_signal = "activate",
+        c_handler = callback.asCPointer().reinterpret(),
+        data = null
+    )
+
+    gtk_application_run(
+        app = app,
+        argc = 0,
+        argv = null
     )
 
     println("Hello world 123!")
