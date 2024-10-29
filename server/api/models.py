bec2ddbcf7a325f08ed14982024685f6e0660e89
Tony Miceli <6946957+wasabinator@users.noreply.github.com>
2024-10-29 15:44:55 +1100

    Allow unauthenticated access to public lessons

--- server/api/models.py
+++ server/api/models.py
@@ -31,23 +31,41 @@ class Lesson(models.Model):
     owner = models.ForeignKey(User, related_name='lesson_owner', on_delete=models.CASCADE)
     language1 = models.ForeignKey(Language, related_name='language1', on_delete=models.CASCADE)
     language2 = models.ForeignKey(Language, related_name='language2', on_delete=models.CASCADE)
+
     VOCABULARY = 0
     GRAMMAR = 1
-    value_types = (
+    types = (
         (VOCABULARY, "Vocabulary"),
         (GRAMMAR, "Grammar"),
     )
-    type = models.IntegerField(choices=value_types, default=0)
+    type = models.IntegerField(choices=types, default=0)
+
+    PRIVATE = 0
+    PUBLIC = 1
+    statuses = (
+        (PRIVATE, "Private"),
+        (PUBLIC, "Public"),
+    )
+    status = models.IntegerField(choices=statuses, default=PRIVATE)
+
+    is_deleted = models.BooleanField(default=False, editable=False)    
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def get_type_name(self):
-        match = [item for item in self.value_types if item[0] == self.type]
+        match = [item for item in self.types if item[0] == self.type]
         if match:
             return match[0][1]
         else:
             return "Grammar"
 
+    def get_status_name(self):
+        match = [item for item in self.statuses if item[0] == self.type]
+        if match:
+            return match[0][1]
+        else:
+            return "Private"
+        
     def __str__(self):
         return f"{self.title} [updated: {self.updated_at}]"
 
