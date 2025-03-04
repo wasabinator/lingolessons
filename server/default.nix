343d35a31a28b24565637b0f7145a5b37e5b82cf
Tony Miceli <6946957+wasabinator@users.noreply.github.com>
2025-03-04 22:45:01 +1100

    Cleanup nix files

--- server/default.nix
+++ server/default.nix
@@ -1,31 +1,29 @@
 { pkgs, devenv, ... }:
 
 {
-#  default = {
-    packages = with pkgs; [
-    ];
+  packages = with pkgs; [
+  ];
 
-    languages = {
-      javascript = {
+  languages = {
+    javascript = {
+      enable = true;
+      npm = {
         enable = true;
-        npm = {
-          enable = true;
-          install.enable = true;
-        };
-      };
-
-      python = {
-        enable = true;
-        venv = {
-          enable = true;
-          requirements = ./requirements.txt;
-        };
+        install.enable = true;
       };
     };
 
-    scripts = {
-      tailwind.exec = "python manage.py tailwind start";
-      run.exec = "python manage.py runserver";
+    python = {
+      enable = true;
+      venv = {
+        enable = true;
+        requirements = ./requirements.txt;
+      };
     };
-#  };
+  };
+
+  scripts = {
+    tailwind.exec = "python manage.py tailwind start";
+    run.exec = "python manage.py runserver";
+  };
 }
