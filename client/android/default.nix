d7535441f8df4ae16ab2ffab5d8d27d70935551e
Tony Miceli <6946957+wasabinator@users.noreply.github.com>
2025-03-04 11:57:23 +0000

    Update nix config with direnv and devenv for server and android (#32)
    * Update nix config with direnv and devenv for server and android

---
+++ client/android/default.nix
@@ -0,0 +1,22 @@
+{ pkgs, devenv, ... }:
+
+{
+  packages = with pkgs; [];
+
+  android = {
+    enable = true;
+    platforms.version = [ "34" ];
+    systemImageTypes = [ "google_apis_playstore" ];
+    emulator = {
+      enable = true;
+      version = "34.1.9";
+    };
+    sources.enable = true;
+    systemImages.enable = true;
+    android-studio.enable = true;
+  };
+
+  languages.java.jdk.package = pkgs.jdk17;
+
+  scripts.create-avd.exec = "avdmanager create avd --force --name 'Pixel6a' --package 'system-images;android-34-ext10;google_apis_playstore;x86_64' --device 'pixel_6a'";
+}
---
+++ server/default.nix
@@ -0,0 +1,29 @@
+{ pkgs, devenv, ... }:
+
+{
+  packages = with pkgs; [
+  ];
+
+  languages = {
+    javascript = {
+      enable = true;
+      npm = {
+        enable = true;
+        install.enable = true;
+      };
+    };
+
+    python = {
+      enable = true;
+      venv = {
+        enable = true;
+        requirements = ./requirements.txt;
+      };
+    };
+  };
+
+  scripts = {
+    tailwind.exec = "python manage.py tailwind start";
+    run.exec = "python manage.py runserver";
+  };
+}
