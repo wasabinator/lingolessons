0bd654111ae26d18cb59fef39e2e205d28dcd5eb
Tony Miceli <6946957+wasabinator@users.noreply.github.com>
2025-07-21 21:55:40 +1000

    Nix env setup: check architecture and create appropriate emulator based on system arch

--- client/default.nix
+++ client/default.nix
@@ -9,12 +9,12 @@
       enable = true;
       version = [ "28.0.13004108" ]; # available versions defined in https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/androidndk-pkgs/default.nix
     };
-    platforms.version = [ "34" "35" ];
-    buildTools.version = [ "34.0.0" "35.0.0" ];
+    platforms.version = [ "35" ];
+    buildTools.version = [ "35.0.0" ];
     systemImageTypes = [ "google_apis_playstore" ];
     emulator = {
       enable = true;
-      version = "34.1.9";
+      version = "35.5.2";
     };
     sources.enable = true;
     systemImages.enable = true;
@@ -54,10 +54,14 @@
   enterShell = ''
       echo 'READY';
       export ANDROID_HOME=$(which android | sed -E 's/(.*libexec\/android-sdk).*/\1/')
-      export ANDROID_NDK_HOME=$ANDROID_HOME/ndk/28.0.13004108
-      export ANDROID_NDKSDK_ROOT=$ANDROID_NDK_HOME
+      export ANDROID_NDK_HOME=$ANDROID_HOME/ndk/28.0.13004108/
+      export ANDROID_NDK_ROOT=$ANDROID_NDK_HOME
       export PATH=$ANDROID_HOME/cmdline-tools/latest/bin:$PATH
   '';
   
-  scripts.create-avd.exec = "avdmanager create avd --force --name 'Pixel6a' --package 'system-images;android-34-ext10;google_apis_playstore;x86_64' --device 'pixel_6a'";
+  scripts.create-avd.exec = if builtins.currentSystem == "aarch64-linux" || builtins.currentSystem == "aarch64-darwin"
+  then "avdmanager create avd --force --name 'Pixel' --package 'system-images;android-35;google_apis_playstore;arm64-v8a' --device 'pixel_6a'"
+  else "avdmanager create avd --force --name 'Pixel' --package 'system-images;android-35;google_apis_playstore;x86_64' --device 'pixel_6a'";
+
+  scripts.avd.exec = "nohup $ANDROID_HOME/emulator/emulator -avd Pixel &";
 }
