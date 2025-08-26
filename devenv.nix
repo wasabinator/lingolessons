3a42cce83419d9639c4e4c58e4104ca9ecd036e4
Tony Miceli <6946957+wasabinator@users.noreply.github.com>
2025-08-26 19:25:13 +1000

    Update rustfmt to nightly

--- devenv.nix
+++ devenv.nix
@@ -1,19 +1,27 @@
 { pkgs, lib, config, inputs, ... }:
 
+let
+  nightly = pkgs.rust-bin.nightly.latest.default.override {
+    extensions = [
+      "rustfmt"
+    ];
+  };
+in
 {
+  cachix.enable = false;
+
+  overlays = [ inputs.rust-overlay.overlays.default ];
+
   packages = with pkgs; [
-    rustup
   ];
 
   languages.rust = {
     enable = true;
-    channel = "stable";
-    components = [ "rustc" "cargo" "rust-std" ];
+    toolchain.rustfmt = nightly;
   };
 
   enterShell = ''
     ln -sf local.properties client/android/local.properties
-    #rustup default stable
   '';
 
   enterTest = ''
@@ -21,6 +29,16 @@
     git --version | grep --color=auto "${pkgs.git.version}"
   '';
 
-  scripts.check.exec = "cargo +nightly clippy";
-  scripts.fix.exec = "cargo +nightly clippy --fix";
+  git-hooks.hooks = {
+    rustfmt.enable = true;
+    clippy = {
+      enable = true;
+      settings = {
+        allFeatures = true;
+        offline = false;
+        denyWarnings = true;
+        extraArgs = "--all-targets";
+      };
+    };
+  };
 }
